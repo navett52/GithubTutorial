68dc47a0c6aa9dc22b8ab737e158cf288ca93ee2
Evan <evantellep@yahoo.com>
2017-07-31 12:14:47 -0400

    Seeting up for a merge conflict

---
+++ src/merge/conflict/Main.java
@@ -0,0 +1,16 @@
+package merge.conflict;
+
+public class Main {
+	
+	public int a = 1;
+	
+	public static void main(String[] args) {
+		
+		Main main = new Main();
+		
+		if (main.a == 1) {
+			System.out.println("We're learning github!");
+		}
+	}
+
+}
